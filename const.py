a499f3271a5e92ae059293d4d24b61352a5cf4f3
Lyfesaver <61745478+lyfesaver74@users.noreply.github.com>
2025-10-02 16:00:10 -0500

    Delete const.py

--- const.py
+++
@@ -1,6 +0,0 @@
-"""Constants for the Dispatcharr Sensor integration."""
-
-DOMAIN = "dispatcharr_sensor"
-
-# List of platforms to support. In this case, just the "sensor" platform.
-PLATFORMS = ["sensor"]
